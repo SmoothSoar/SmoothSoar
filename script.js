79600ffa7cc1dae7e70dfe7dc8c3b10d0e86b121
SmoothSoar <142686511+SmoothSoar@users.noreply.github.com>
2023-08-22 19:49:25 -0400

    fixes

---
+++ script.js
@@ -0,0 +1,11 @@
+TweenMax.from("#desc",1,{opacity:0, y: 60});
+
+const tl = new TimelineMax({repeat: -1});
+const rainText = document.querySelectorAll(".rain-text");
+
+rainText.forEach(rain=>{
+  const ani1 = TweenMax.to(rain, 3,{opacity:1,ease:Power4.easeInOut, fontSize : 4.5+"rem"},1 );
+  const ani2 =TweenMax.to(rain, 2, {opacity:0, ease:Power4.easeInOut,  rotationX: 110, transformOrigin: "center bottom"},1);
+  tl.add(ani1);
+  tl.add(ani2);
+})
